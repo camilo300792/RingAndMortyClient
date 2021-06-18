146051333270ea783d1b4ce65bc9cdb9ed2c4f6e
mono0416 <dnjonathanbeltran@gmail.com>
2021-06-18 11:24:14 -0500

    creacion de base de datos en character

---
+++ samples/characters.js
@@ -0,0 +1,18 @@
+form = document.getElementById('form-characters')
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let formData = new FormData(e.target)
+    fetch('characters.php?character=' + formData.get('character'))
+        .then(response => response.json())
+        .then(respJson =>
+        {
+            document.getElementById('name').innerHTML = respJson.name;
+            document.getElementById('status').innerHTML = respJson.status;
+            document.getElementById('species').innerHTML = respJson.species;
+            document.getElementById('gender').innerHTML = respJson.gender;
+            
+            
+        });
+
+
+})
--- samples/characters.php
+++ samples/characters.php
@@ -1,11 +1,43 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/database.php';
 
 $ramClient = new \RAMC\RAMClient();
 
-$response = $ramClient->getCharacter($_GET['character']);
+$query = <<<EOD
+SELECT * FROM characters WHERE id = ?
+EOD;
 
-echo $response;
+$queryInsert = <<<EOD
+INSERT INTO characters (id, `name`, status, species, type, gender)
+VALUES (?, ?, ?, ?, ?, ?)
+EOD;
+
+$stmt = $connection->prepare($query);
+$stmtInsert = $connection->prepare($queryInsert);
+
+if (is_numeric($_GET['character']) && is_int((int) $_GET['character'])) {
+    $stmt->execute([$_GET['character']]);
+    if (!$result = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        $response = $ramClient->getCharacter($_GET['character']);
+        $responseDecoded = json_decode($response, true);
+        $stmtInsert->execute([
+            $responseDecoded['id'],
+            $responseDecoded['name'],
+            $responseDecoded['status'],
+            $responseDecoded['species'],
+            $responseDecoded['type'],
+            $responseDecoded['gender']
+            
+        ]);
+        echo $response;
+    } else {
+        echo json_encode($result);
+    }
+} else {
+    echo 'El id del personage debe ser un número entero';
+}
+;
 
 
